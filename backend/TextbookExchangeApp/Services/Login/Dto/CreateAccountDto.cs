73311daf91e70073fc7828c116ebf5607afbb4eb
jackson <jacksonwaffle1@gmail.com>
2025-04-01 15:42:44 -0500

    Email validation

--- backend/TextbookExchangeApp/Services/Login/Dto/CreateAccountDto.cs
+++ backend/TextbookExchangeApp/Services/Login/Dto/CreateAccountDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TextbookExchangeApp.Services.Login.Dto;
 
 public class CreateAccountDto
 {
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
     public string Email { get; set; }
     public string Password { get; set; }    
 }
