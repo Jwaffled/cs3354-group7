538d87211abee30d8ea13fe2555387dce55bb058
Your Name <you@example.com>
2025-04-01 22:51:32 -0500

    Added specific folders and placeholder files.

---
+++ backend/TextbookExchangeApp.Tests/ReplyController/ReplyController_CreateReply_Tests.cs
@@ -0,0 +1,8 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
+namespace TextbookExchangeApp.Tests;
