a4b994cee263a50349d5b8000ef453ad7cefb73f
jackson <jacksonwaffle1@gmail.com>
2025-04-10 15:14:40 -0500

    Add shadcn, update project structure

--- frontend/textbook-exchange-app/src/App.jsx
+++ frontend/textbook-exchange-app/src/App.jsx
@@ -1,24 +1,10 @@
 import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        {/* Routes would go here, example below: */}
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} /> */}
-      </Routes>
+      <Home />
     </div>
   );
 }
---
+++ frontend/textbook-exchange-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Test button</Button>
+      <h1 class="text-3xl font-bold underline">Hello, world!</h1>
+    </div>
+  );
+}
